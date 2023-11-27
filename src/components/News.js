d80c70ba44f9bd3f3702b18ecb2b1412970d691d
NIKHIL AGRAWAL <nagrawal203@gmail.com>
2023-11-27 18:36:49 +0530

    Vid34Comp

--- src/components/News.js
+++ src/components/News.js
@@ -15,18 +15,23 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  constructor() {
-    super();
-    // console.log("Hello! I am a Constructor from a NEWS Component!")
+  capitaliseFirstLetter = (string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
 
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+
+    document.title = `${this.capitaliseFirstLetter(this.props.category)} - NewsMannki`;
   }
 
-  async componentDidMount() {
+  async updateNews() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2c93bc1669204e2da842b657636d84c9&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -39,45 +44,27 @@ export class News extends Component {
     });
   }
 
+  async componentDidMount() {
+    this.updateNews();
+  }
+
   handlePrevClick = async () => {
-    // console.log("Previous");
-
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2c93bc1669204e2da842b657636d84c9&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
-
-    // console.log(parsedData);
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   };
 
   handleNextClick = async () => {
-    // console.log("Next");
-    if (!(this.state.page + 1 >Math.ceil(this.state.totalResults / this.props.pageSize))) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2c93bc1669204e2da842b657636d84c9&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      // console.log(parsedData);
-
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
-    }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   };
 
   render() {
     return (
       <>
         <div className="container my-3">
-          <h1 className="text-center" style={{margin: '30px 0px'}}>NewsMannKi - Top Headlines</h1>
+          <h1 className="text-center" style={{ margin: "30px 0px" }}>
+            NewsMannKi - Top Headlines Of {this.capitaliseFirstLetter(this.props.category)}
+          </h1>
 
           {this.state.loading && <Spinner />}
 
@@ -89,9 +76,11 @@ export class News extends Component {
                     <NewsItem
                       title={element.title ? element.title.slice(0, 45) : ""}
                       description={
-                        element.description ? element.description.slice(0, 90) : ""
+                        element.description
+                          ? element.description.slice(0, 90)
+                          : ""
                       }
-                      imageUrl={element.urlToImage} 
+                      imageUrl={element.urlToImage}
                       newsUrl={element.url}
                       author={element.author}
                       date={element.publishedAt}
@@ -103,10 +92,23 @@ export class News extends Component {
           </div>
         </div>
         <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-info mb-5 mx-3" onClick={this.handlePrevClick} >
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-info mb-5 mx-3"
+            onClick={this.handlePrevClick}
+          >
             &larr; Previous
           </button>
-          <button disabled={this.state.page + 1 >Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-info mb-5 mx-3" onClick={this.handleNextClick} >
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-info mb-5 mx-3"
+            onClick={this.handleNextClick}
+          >
             Next &rarr;
           </button>
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text"> {description}... </p>
-            <p className="card-text"><small className="text-light">By {!author?"Unknown":author} on {new Date(date).toGMTString()} </small></p>
+            <p className="card-text"><small className="text-secondary">By {!author?"Unknown":author} on {new Date(date).toGMTString()} </small></p>
             <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-warning"> Read More </a>
           </div>
         </div>
